e30615ac3805252b1dc98c545c41668e9612e2ed
Joseph Hunkeler <jhunkeler@gmail.com>
2010-12-06 15:31:40 -0500

    License text

--- src/misc.c
+++ src/misc.c
@@ -1,3 +1,23 @@
+/**
+ *  NetNuke - Erases all storage media detected by the system
+ *  Copyright (C) 2009-2010  Joseph Hunkeler <jhunkeler@gmail.com, jhunk@stsci.edu>
+ *
+ *  This file is part of NetNuke.
+ *
+ *  NetNuke is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NetNuke is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NetNuke.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 #include <string.h>
 
 /* Returns the index of the first occurence of ch in str */
